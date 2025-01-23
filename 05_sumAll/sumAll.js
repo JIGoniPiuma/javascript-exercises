5af387fec6a3824c50ce7947a7f376f99bb94057
JIGP <jgonipiuma@gmail.com>
2025-01-23 17:49:57 -0300

    sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
+const sumAll = function (posInt1, posInt2) {
+  let sum = 0;
+  let distance = Math.abs(posInt1 - posInt2);
+  let min = Math.min(posInt1, posInt2);
 
+  if (
+    !Number.isInteger(posInt1) ||
+    posInt1 <= 0 ||
+    !Number.isInteger(posInt2) ||
+    posInt2 <= 0
+  ) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i <= distance; i++) {
+      sum += min;
+      min++;
+    }
+
+    return sum;
+  }
 };
-
+sumAll(1, 4000);
+sumAll(123, 1);
+sumAll(-10, 4);
+sumAll(2.5, 4);
 // Do not edit below this line
 module.exports = sumAll;
