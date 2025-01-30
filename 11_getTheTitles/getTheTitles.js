1e0087e454b1fd00805b97642123849af9ce30c2
JIGP <jgonipiuma@gmail.com>
2025-01-30 17:36:01 -0300

    finish  getTitles &findTheOldest

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
 
+console.log(books);
+const getTheTitles = function (arr) {
+  console.log(arr.map((item) => item.title));
+  return arr.map((item) => item.title);
 };
-
+getTheTitles(books);
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
 
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+
+const currentYear = new Date().getFullYear();
+
+const findTheOldest = function (people) {
+  people.reduce((oldest, person) => {
+    let ageOldest = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+    console.log(ageOldest);
+    let agePerson = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+    console.log(agePerson);
+    console.log(ageOldest > agePerson ? oldest : person);
+    return ageOldest > agePerson ? oldest : person;
+  });
 };
+findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
